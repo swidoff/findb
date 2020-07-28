7f52da9ab85d35f2ebc53df85997f1ec7b0268dd
Seth W <swidoff@gmail.com>
2020-07-27 22:51:51 -0700

    Test using an mmaped IPC file as a record batch source.

--- tests/test_latest_prices.rs
+++ tests/test_latest_prices.rs
@@ -1,13 +1,21 @@
 use std::fs::File;
+use std::io::{IoSliceMut, Read, Seek, SeekFrom};
+use std::io;
 use std::sync::Arc;
 
 use arrow::array::{BooleanArray, Float64Array, StringArray, UInt32Array, UInt64Array};
 use arrow::compute::kernels::{boolean, comparison, filter};
 use arrow::csv::Reader;
 use arrow::datatypes::{DataType, Field, Schema};
+use arrow::ipc::reader::FileReader;
+use arrow::ipc::writer::FileWriter;
 use arrow::record_batch::RecordBatch;
 use arrow::util::pretty::print_batches;
 use itertools::Itertools;
+use memmap::{Mmap, MmapOptions};
+use arrow::record_batch::RecordBatchReader;
+
+use findb::Query;
 
 fn schema() -> Schema {
     Schema::new(vec![
@@ -47,7 +55,7 @@ fn test_one_name_one_date() {
         &comparison::eq_scalar(&date_column, target_date).unwrap(),
         &comparison::eq_utf8_scalar(&fid_column, target_name).unwrap(),
     )
-    .unwrap();
+        .unwrap();
 
     let res = filter::filter(close_column, &condition).unwrap();
     let schema = Schema::new(vec![Field::new("close", DataType::Float64, true)]);
@@ -74,14 +82,14 @@ fn test_two_names_two_dates() {
             &comparison::eq_scalar(&date_column, target_date1).unwrap(),
             &comparison::eq_scalar(&date_column, target_date2).unwrap(),
         )
-        .unwrap(),
+            .unwrap(),
         &boolean::or(
             &comparison::eq_utf8_scalar(&fid_column, target_name1).unwrap(),
             &comparison::eq_utf8_scalar(&fid_column, target_name2).unwrap(),
         )
-        .unwrap(),
+            .unwrap(),
     )
-    .unwrap();
+        .unwrap();
 
     let schema = Schema::new(vec![
         Field::new("date", DataType::UInt32, false),
@@ -96,101 +104,16 @@ fn test_two_names_two_dates() {
             filter::filter(close_column, &condition).unwrap(),
         ],
     )
-    .unwrap();
+        .unwrap();
 
     let batch_arr = [batch];
     print_batches(&batch_arr).unwrap()
 }
 
-struct Query {
-    build_date: u32,
-    start_date: u32,
-    end_date: u32,
-    eff_timestamp: u64,
-    asset_ids: Vec<String>,
-}
-
-impl Query {
-    fn query_date_range(&self, date_column: &UInt32Array) -> BooleanArray {
-        boolean::and(
-            &comparison::gt_eq_scalar(date_column, self.start_date).unwrap(),
-            &comparison::lt_eq_scalar(date_column, self.end_date).unwrap(),
-        )
-        .unwrap()
-    }
-
-    fn query_asset_ids(&self, asset_id_column: &StringArray) -> BooleanArray {
-        let mut asset_id_condition =
-            comparison::eq_utf8_scalar(asset_id_column, &self.asset_ids[0][..]).unwrap();
-
-        for id in self.asset_ids.iter().dropping(1) {
-            let eq_cond = comparison::eq_utf8_scalar(asset_id_column, id).unwrap();
-            asset_id_condition = boolean::or(&asset_id_condition, &eq_cond).unwrap()
-        }
-        asset_id_condition
-    }
-
-    fn query_eff_timestamp(
-        &self,
-        eff_start_column: &UInt64Array,
-        eff_end_column: &UInt64Array,
-    ) -> BooleanArray {
-        boolean::and(
-            &comparison::lt_eq_scalar(eff_start_column, self.eff_timestamp).unwrap(),
-            &comparison::gt_eq_scalar(eff_end_column, self.eff_timestamp).unwrap(),
-        )
-        .unwrap()
-    }
-
-    pub fn query(&self, batch: &RecordBatch) -> RecordBatch {
-        let date_column: &UInt32Array = get_column(&batch, 0);
-        let fid_column: &StringArray = get_column(&batch, 1);
-        let eff_start_column: &UInt64Array = get_column(&batch, 2);
-        let eff_end_column: &UInt64Array = get_column(&batch, 3);
-        let close_column: &Float64Array = get_column(&batch, 4);
-
-        let condition = boolean::and(
-            &boolean::and(
-                &self.query_date_range(date_column),
-                &self.query_asset_ids(fid_column),
-            )
-            .unwrap(),
-            &self.query_eff_timestamp(eff_start_column, eff_end_column),
-        )
-        .unwrap();
-
-        let res_schema = Schema::new(vec![
-            Field::new("build_date", DataType::UInt32, false),
-            Field::new("fid", DataType::Utf8, false),
-            Field::new("data_date", DataType::UInt32, false),
-            Field::new("close", DataType::Float64, true),
-        ]);
-
-        let res_date = filter::filter(date_column, &condition).unwrap();
-        let res_fid = filter::filter(fid_column, &condition).unwrap();
-        let res_close = filter::filter(close_column, &condition).unwrap();
-
-        let len = res_date.len();
-        let mut build_date_column_builder = UInt32Array::builder(len);
-        for _ in 0..len {
-            build_date_column_builder
-                .append_value(self.build_date)
-                .unwrap();
-        }
-        let res_build_date = Arc::new(build_date_column_builder.finish());
-
-        RecordBatch::try_new(
-            Arc::new(res_schema),
-            vec![res_build_date, res_fid, res_date, res_close],
-        )
-        .unwrap()
-    }
-}
-
 #[test]
 fn test_query_list() {
     let batch = read_faangm_20206_close();
-    let query_list = [
+    let query_list = vec![
         Query {
             build_date: 20200624,
             start_date: 20200619,
@@ -207,6 +130,92 @@ fn test_query_list() {
         },
     ];
 
-    let batch_arr = [query_list[0].query(&batch), query_list[1].query(&batch)];
+    let batch_arr = Query::query_all(&query_list, &batch, 4).unwrap();
     print_batches(&batch_arr[..]).unwrap()
+}
+
+#[test]
+fn test_mmaped_db() {
+    let schema = schema();
+    let batch = read_faangm_20206_close();
+    let ipc_file = File::create("tests/content/faangm_202006_close.ipc").unwrap();
+    let mut writer = FileWriter::try_new(ipc_file, &schema).unwrap();
+    writer.write(&batch).unwrap();
+    writer.finish().unwrap();
+
+    let ipc_file = File::open("tests/content/faangm_202006_close.ipc").unwrap();
+    let mmap_file = MmapFile::new(ipc_file);
+    let mut reader = FileReader::try_new(mmap_file).unwrap();
+    println!("Num batches: {}", reader.num_batches());
+
+    let batch = reader.next_batch().unwrap().unwrap();
+
+    let query_list = vec![
+        Query {
+            build_date: 20200624,
+            start_date: 20200619,
+            end_date: 20200623,
+            eff_timestamp: 1595807440,
+            asset_ids: vec!["AMZN".to_string(), "MSFT".to_string()],
+        },
+        Query {
+            build_date: 20200625,
+            start_date: 20200622,
+            end_date: 20200624,
+            eff_timestamp: 1595807440,
+            asset_ids: vec!["NTFZ".to_string(), "AAPL".to_string()],
+        },
+    ];
+
+    let batch_arr = Query::query_all(&query_list, &batch, 4).unwrap();
+    print_batches(&batch_arr[..]).unwrap()
+}
+
+struct MmapFile {
+    mmap: Mmap,
+    offset: u64,
+}
+
+impl MmapFile {
+    fn new(file: File) -> MmapFile {
+        let mmap = unsafe { MmapOptions::new().map(&file).unwrap() };
+        MmapFile { mmap, offset: 0 }
+    }
+
+    fn to_arr(&self) -> &[u8] {
+        &self.mmap[self.offset as usize..]
+    }
+}
+
+impl Seek for MmapFile {
+    fn seek(&mut self, pos: SeekFrom) -> io::Result<u64> {
+        match pos {
+            SeekFrom::Start(offset) => self.offset = offset,
+            SeekFrom::End(offset) => self.offset = (self.mmap.len() as i64 + offset) as u64,
+            SeekFrom::Current(offset) => self.offset = (self.offset as i64 + offset) as u64,
+        }
+        Ok(self.offset)
+    }
+}
+
+impl Read for MmapFile {
+    #[inline]
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        self.to_arr().read(buf)
+    }
+
+    #[inline]
+    fn read_vectored(&mut self, bufs: &mut [IoSliceMut<'_>]) -> io::Result<usize> {
+        self.to_arr().read_vectored(bufs)
+    }
+
+    #[inline]
+    fn read_to_end(&mut self, buf: &mut Vec<u8>) -> io::Result<usize> {
+        self.to_arr().read_to_end(buf)
+    }
+
+    #[inline]
+    fn read_exact(&mut self, buf: &mut [u8]) -> io::Result<()> {
+        self.to_arr().read_exact(buf)
+    }
 }

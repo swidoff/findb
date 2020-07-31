2ac8b6be10a9c165a91438e504de23b313487fba
Seth W <swidoff@gmail.com>
2020-07-31 10:06:13 -0700

    Script to prep the csv files for loading by convert dates to integers and sorting the files so they can be merged later.

---
+++ scripts/convert-csv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if (($# < 2)); then
+  echo "Two arguments required; source file and target file"
+fi
+
+source_file=$1
+target_file=$2
+
+python3 convert-datetimes.py -d 0 -t 3 4 <"$source_file" | sort -t , -k 1,2 >"$target_file"

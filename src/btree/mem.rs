1a7f41036a21fbe9e5552861793209b8cb8e0f94
Seth W <swidoff@gmail.com>
2020-10-27 22:04:27 -0700

    Split for leaf node.

--- src/btree/mem.rs
+++ src/btree/mem.rs
@@ -3,32 +3,68 @@ pub struct BTree {
     root: Box<dyn Node>,
 }
 
+enum InsertResult {
+    SuccessNoSplit,
+    Duplicate,
+    SuccessSplit {
+        midpoint_key: u32,
+        split_node: Box<dyn Node>,
+    },
+}
+
 trait Node {
     fn find_node(&self, key: u32) -> Option<&Leaf>;
+    fn insert(&mut self, key: u32, value: u32) -> InsertResult;
 }
 
 struct Leaf {
-    keys_and_values: Vec<(u32, u32)>,
+    kv: Vec<(u32, u32)>,
 }
 
 impl Node for Leaf {
     fn find_node(&self, key: u32) -> Option<&Leaf> {
-        if !self.keys_and_values.is_empty()
-            && key >= self.keys_and_values[0].1
-            && key <= self.keys_and_values[self.keys_and_values.len() - 1].1
-        {
+        if !self.kv.is_empty() && key >= self.kv[0].1 && key <= self.kv[self.kv.len() - 1].1 {
             Option::Some(&self)
         } else {
             Option::None
+        }
+    }
+
+    fn insert(&mut self, key: u32, value: u32) -> InsertResult {
+        match self.kv.binary_search_by_key(&key, |value| value.1) {
+            Ok(_) => InsertResult::Duplicate,
+            Err(index) => {
+                if self.kv.len() < self.kv.capacity() {
+                    self.kv.insert(index, (key, value));
+                    InsertResult::SuccessNoSplit
+                } else {
+                    let midpoint_index = self.kv.len() / 2;
+
+                    // Allocate new kv for split node, copying from the midpoint of this node's kv.
+                    let mut split_kv = Vec::with_capacity(self.kv.capacity());
+                    for i in midpoint_index..self.kv.len() {
+                        split_kv.push(self.kv[i])
+                    }
+
+                    // Truncate this kv from the midpoint and push the new key and value.
+                    self.kv.truncate(midpoint_index);
+                    self.kv.push((key, value));
+
+                    InsertResult::SuccessSplit {
+                        midpoint_key: self.kv[midpoint_index].1,
+                        split_node: Box::new(Leaf { kv: split_kv }),
+                    }
+                }
+            }
         }
     }
 }
 
 impl Leaf {
     fn lookup(&self, key: u32) -> Option<u32> {
-        self.keys_and_values
+        self.kv
             .binary_search_by_key(&key, |value| value.1)
-            .map(|idx| self.keys_and_values[idx].1)
+            .map(|idx| self.kv[idx].1)
             .ok()
     }
 }
@@ -38,13 +74,24 @@ struct InternalNode {
     pointers: Vec<Box<dyn Node>>,
 }
 
+impl InternalNode {
+    fn child_for(&self, key: u32) -> &Box<dyn Node> {
+        let index = match self.keys.binary_search(&key) {
+            Ok(index) => index + 1,
+            Err(index) => index,
+        };
+        &self.pointers[index]
+    }
+}
+
 impl Node for InternalNode {
     fn find_node(&self, key: u32) -> Option<&Leaf> {
-        let child_node = match self.keys.binary_search(&key) {
-            Ok(index) => &self.pointers[index + 1],
-            Err(index) => &self.pointers[index],
-        };
-        child_node.find_node(key)
+        self.child_for(key).find_node(key)
+    }
+
+    fn insert(&mut self, key: u32, value: u32) -> InsertResult {
+        // self.child_for(key).insert(key, value)
+        unimplemented!()
     }
 }
 
@@ -53,7 +100,7 @@ impl BTree {
         BTree {
             capacity,
             root: Box::new(Leaf {
-                keys_and_values: Vec::with_capacity(capacity),
+                kv: Vec::with_capacity(capacity),
             }),
         }
     }
@@ -66,7 +113,9 @@ impl BTree {
         panic!("Not implemented")
     }
 
-    pub fn insert(&self, key: u32, value: u32) {}
+    pub fn insert(&self, key: u32, value: u32) -> bool {
+        return false;
+    }
 
     pub fn update(&self, key: u32, value: u32) -> bool {
         return false;
